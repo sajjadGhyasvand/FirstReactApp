abd981a3801a58b3df104b462f79146ead82741a
sajjadGhyasvand <Sajjadgh1995@hotmail.com>
2024-03-28 23:03:14 +0330

    Add Bootstrap , Fonts Ana FontAwsome

--- contact-manager-app/src/App.js
+++ contact-manager-app/src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <h3>مدیریت مخاطبین</h3>
+      <h3 >مدیریت مخاطبین</h3>
+
+        <button className='btn btn-secondary'>
+          <span className="fa fa-user fa-2x px-2"></span>
+          بزن بریم!
+        </button>
     </div>
   );
 }
