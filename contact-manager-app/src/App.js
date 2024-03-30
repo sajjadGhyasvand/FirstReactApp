d4bc498bcda56de7eef9aa49cfb5288900b2b95b
sajjadGhyasvand <Sajjadgh1995@hotmail.com>
2024-03-30 17:08:32 +0330

    Add Routing

--- contact-manager-app/src/App.js
+++ contact-manager-app/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
-import {AddContact,EditContact,ViewContact,Contacts,Contact,Navbar} from "./Components";
+import {AddContact,EditContact,Contacts,Contact,Navbar} from "./Components";
 import  {useState} from "react";
-
+import {Routes,Route,Navigate} from 'react-router-dom'
 const App = () => {
     const [getContacts,setContacts] = useState([]);
     const [loading,setLoading] = useState(false);
   return (
     <div className="App">
       <Navbar />
-      <Contacts contacts={getContacts} loading={loading}/>
+        <Routes>
+           <Route path="/" element={<Navigate to="/contacts" />} />
+           <Route path="/contacts" element={<Contacts contacts={getContacts} loading={loading}/>} />
+           <Route path="/contacts/:contactId" element={<Contact />} />
+            <Route path="/contacts/add" element={<AddContact/>} />
+            <Route path="/contacts/edit/:contactId" element={<EditContact/>} />
+        </Routes>
+
     </div>
   );
 }
