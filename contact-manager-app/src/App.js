a1d999fbf460e755f5016e6126f71e8b6b1d0524
sajjadGhyasvand <Sajjadgh1995@hotmail.com>
2024-03-31 20:26:11 +0330

    Create API and Add Contact

--- contact-manager-app/src/App.js
+++ contact-manager-app/src/App.js
@@ -1,10 +1,55 @@
 import './App.css';
 import {AddContact,EditContact,Contacts,Contact,Navbar} from "./Components";
-import  {useState} from "react";
-import {Routes,Route,Navigate} from 'react-router-dom'
+import  {useState,useEffect} from "react";
+import {Routes,Route,Navigate,useNavigate} from 'react-router-dom';
+import {createContact, getAllContacts, getAllGroups} from "./Services/contactService";
+import axios from "axios";
 const App = () => {
     const [getContacts,setContacts] = useState([]);
     const [loading,setLoading] = useState(false);
+    const  [getGroups,setGroups] = useState([]);
+    const  [getContact,setContact] = useState({
+        fullname: "",
+        photo:"",
+        mobile:"",
+        email:"",
+        job:"",
+        group:"",
+    });
+const  navigate = useNavigate();
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                setLoading(true);
+                const {data : contactData} =await getAllContacts();
+                const {data : groupsData} = await  getAllGroups();
+                setContacts(contactData);
+                setGroups(groupsData);
+                setLoading(false);
+            }catch (err){
+                console.log(err.message);
+                setLoading(false);
+            }
+        }
+        fetchData();
+    }, []);
+    const createContactForm = async (event) => {
+        event.preventDefault();
+        try{
+            const {status} = await createContact(getContact);
+            if (status === 201){
+                setContact({});
+                navigate("/contacts");
+            }
+        }catch(err){
+            console.log(err.message);
+        }
+    }
+    const setContactInfo = (event) => {
+        setContact({... getContact,
+            [event.target.name] : event.target.value,
+        });
+    }
   return (
     <div className="App">
       <Navbar />
@@ -12,8 +57,13 @@ const App = () => {
            <Route path="/" element={<Navigate to="/contacts" />} />
            <Route path="/contacts" element={<Contacts contacts={getContacts} loading={loading}/>} />
            <Route path="/contacts/:contactId" element={<Contact />} />
-            <Route path="/contacts/add" element={<AddContact/>} />
-            <Route path="/contacts/edit/:contactId" element={<EditContact/>} />
+           <Route path="/contacts/add" element={<AddContact
+               loading={loading}
+               setContactInfo={setContactInfo}
+               contact={getContact}
+               groups={getGroups}/>}
+               createContactForm = {createContactForm}/>
+           <Route path="/contacts/edit/:contactId" element={<EditContact/>} />
         </Routes>
 
     </div>
--- contact-manager-app/src/Components/Contact/Contacts.jsx
+++ contact-manager-app/src/Components/Contact/Contacts.jsx
@@ -3,6 +3,7 @@ import {CurrentLine, Pink, Orange} from "../../Helpers/colors";
 import  NotFound from "../../Assets/no-found.gif";
 import contact from "./Contact";
 import Spinner from "../Spinner";
+import {Link} from "react-router-dom";
 const Contacts = ({contacts, loading}) => {
     return(
         <>
@@ -10,30 +11,34 @@ const Contacts = ({contacts, loading}) => {
                <div className="row">
                    <div className="col">
                        <p className="h3 mt-2">
-                           <button className="btn mx-2" style={{backgroundColor:Pink}}>
+                           <Link to={"/contacts/add"} className="btn mx-2" style={{backgroundColor:Pink}}>
                                ساخت مخاطب جدید
                                <i className="fas fa-plus-circle mx-2"></i>
-                           </button>
+                           </Link>
                        </p>
                    </div>
                </div>
            </section>
             {
                 loading ? <Spinner /> : (
-                    <section className="container">
-                        {
-                            contacts.length > 0 ? contacts.map(c => (
-                                    <Contact key={c.id} contact={c}/>
-                                )) :
-                                (
-                                    <div className="text-center py-5" style={{backgroundColor: CurrentLine}}>
-                                        <p className="h3 " style={{color: Orange}}>
-                                            مخاطب یافت نشد
-                                        </p>
-                                        <img src={NotFound} alt="پیدا نشده" className="w-25"/>
-                                    </div>
-                                )
-                        }
+                    <section className="container row">
+
+                            {
+                                contacts.length > 0 ? contacts.map(c => (
+
+                                        <Contact key={c.id} contact={c}/>
+
+                                    )) :
+                                    (
+                                        <div className="text-center py-5" style={{backgroundColor: CurrentLine}}>
+                                            <p className="h3 " style={{color: Orange}}>
+                                                مخاطب یافت نشد
+                                            </p>
+                                            <img src={NotFound} alt="پیدا نشده" className="w-25"/>
+                                        </div>
+                                    )
+                            }
+
                     </section>
                 )
             }
