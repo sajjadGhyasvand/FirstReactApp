f44b64abfab877105b112300232980817eac4d87
sajjadGhyasvand <Sajjadgh1995@hotmail.com>
2024-04-06 19:58:41 +0330

    Online On Github

--- contact-manager-app/src/index.js
+++ contact-manager-app/src/index.js
@@ -8,7 +8,7 @@ import 'react-toastify/dist/ReactToastify.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename="FirstReactApp">
          <App />
       </BrowserRouter>
   </StrictMode>
